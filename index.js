2f0f3fb77baabdf5556b1654aad046a7c2c17681
victor8653 <96753660+victor8653@users.noreply.github.com>
2024-12-09 16:01:00 -0500

    Created boilerplate, Yeah

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("dotenv").config;
 
 config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
